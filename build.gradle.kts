4c1ec3b725b9a892b77c7303f3f0be7c24ee6aa8
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2020-09-18 10:13:53 +0300

    Convert to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
+}
+
+group = "eu.mikroskeem.zentria"
+version = "1.1.0"
+
+val paperApiVersion = "1.16.3-R0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven("https://papermc.io/repo/repository/maven-public")
+}
+
+dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:$paperApiVersion")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+bukkit {
+    name = "CleanroomGenerator"
+    main = "io.nv.bukkit.CleanroomGenerator.CleanroomGenerator"
+    authors = listOf("NVX (Neo_Vortex)")
+    description = "Custom world generator to generate flat clean rooms. y==0 is bedrock layer=height,block[:datavalue] generator id=layer[|layer[|...]] - Prefix . at the start of the ID to skip bedrock generation at layer0"
+    load = net.minecrell.pluginyml.bukkit.BukkitPluginDescription.PluginLoadOrder.STARTUP
+    website = "https://dev.bukkit.org/projects/cleanroomgenerator"
+}
